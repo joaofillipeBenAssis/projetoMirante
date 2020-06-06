f708a138a26ae5a1809e4c309e36535700dcf3af
joaofillipeBenAssis <joaofillipe.ben.assis@gmail.com>
2020-06-06 16:41:26 -0300

    inclusão de repository

---
+++ src/main/java/ProjetoMirante/repository/RoleRepository.java
@@ -0,0 +1,19 @@
+package ProjetoMirante.repository;
+
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import ProjetoMirante.entidades.Operador;
+import ProjetoMirante.entidades.Role;
+
+public interface RoleRepository extends CrudRepository<Role, Long>
+{
+    @Query("Select o from Operador o where o.login = :login")
+    Operador buscarOperador(@Param("login") String login);
+
+    @Query("Select o from Operador o order by o.nome asc")
+    ArrayList<Operador> buscarOperadores();
+}
